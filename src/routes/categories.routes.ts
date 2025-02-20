385ae2dbacd9460d592bc79f2503f409b31f31ba
Inácio Rodrigues <inaciorodrigues395@gmail.com>
2025-02-20 17:13:54 +0100

    feature Category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,28 @@
 import { Router } from 'express';
-import Response from 'express';
-import { v4 as uuidv4 } from 'uuid';
- const CategoriesRouter = Router();
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
-const categories = [];
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
- CategoriesRouter.post('/', (request, response) => {
-    
-    const { name, description } = request.body;
+categoriesRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
 
-    const category ={
-        id: uuidv4(),
-        name,
-        description
-    };
-    categories.push(category);
-    return response.status(201).send();
- });
+  const categoriesAlrightExists = categoriesRepository.findByName(name);
+  if (categoriesAlrightExists) {
+    return response.status(400).json({ error: 'Category already exists!' });
+  }
+ 
+
+  categoriesRepository.create({ name, description });
+
+  return response.status(201).send();
+});
 
 
- export { CategoriesRouter };
+categoriesRoutes.get('/', (request, response) => {
+  const all = categoriesRepository.list();
+  return response.json(all);
+}
+);
+
+export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import { CategoriesRouter } from './routes/categories.routes';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/categories", CategoriesRouter);
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => {
   console.log("Server is running on port 3000");
